62463f9835b0f65891d39d1430f42eb5d6887c1d
ben-teachtci <bangel@teachtci.com>
2014-03-22 11:14:03 -0700

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GmailDb::Application.config.secret_key_base = 'ba972527fae43601295e3c292b9ee277c439bad0231fe031eea334067b83980389f54c5306e85d3f3afad8c01fa613c73c69afcd4e4b267560fd3ff1c7641567'
