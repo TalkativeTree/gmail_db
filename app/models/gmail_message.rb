92f01310d6f0ce2404a5d917db4eb04ad77a3bdd
ben-teachtci <bangel@teachtci.com>
2014-03-22 23:23:12 -0700

    add GmailMessage.sent_by scope and GmailMessage#body

--- app/models/gmail_message.rb
+++ app/models/gmail_message.rb
@@ -1,4 +1,10 @@
 class GmailMessage < ActiveRecord::Base
   has_many :parts
   belongs_to :admin
+
+  scope :sent_by, ->(user){ where(from: user.email) }
+
+  def body
+    parts.select{|part| part.content.include?("MIME-Version: 1.0")}[0].content
+  end
 end
