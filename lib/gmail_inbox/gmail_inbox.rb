d3b07d7d96ddce82fff4b0b2f34f35c00f55052c
ben-teachtci <bangel@teachtci.com>
2014-03-22 23:15:42 -0700

    refactor saving gmail messages into EmailSaver and wrap GmailInbox & EmailSaver inside EmailRetriever module

--- lib/gmail_inbox/gmail_inbox.rb
+++ lib/gmail_inbox/gmail_inbox.rb
@@ -1,46 +1,88 @@
-class GmailInbox
-  attr_accessor :gmail, :user
+module EmailRetriever
 
-  def initialize(user_name, user_password)
-    @user  = user_name
-    @gmail = Gmail.new(user_name, user_password)
+  def fetch_emails(user)
+    inbox = GmailInbox.new(user.email, user.gmail_password)
+    messages = inbox.all(:all, after:  Net::IMAP.format_datetime(5.minutes.ago))
+    EmailSaver.save(messages)
   end
 
-  def received
-    @received ||= gmail.mailbox('INBOX').emails
-    @received
+
+  class GmailInbox
+    require 'gmail'
+
+    attr_accessor :gmail, :user
+
+    def initialize(user_name, user_password)
+      @user  = user_name
+      @gmail = Gmail.new(user_name, user_password)
+    end
+
+    def received(key_or_opts = :all, opts={})
+      gmail.mailbox('INBOX').emails(key_or_opts, opts)
+    end
+
+    def sent(key_or_opts = :all, opts={})
+      gmail.mailbox('[Gmail]/Sent Mail').emails(key_or_opts, opts)
+    end
+
+    # all only returns INBOX and Sent Mail
+    def all(key_or_opts = :all, opts={})
+      process(received(key_or_opts, opts)) + process(sent(key_or_opts, opts))
+    end
+
   end
 
-  def sent
-    @sent ||= gmail.mailbox('[Gmail]/Sent Mail').emails
-    @sent
-  end
+  class EmailSaver
+    require 'sanitize'
 
-  def fetch(group)
-    case group
-    when :all
-      process(received)
-      process(sent)
-    when :recieved
-      process(recieved)
-    when :sent
-      process(sent)
+    class << self
+      def process(messages)
+        messages.each do |email|
+          begin
+            gmail_message = if defined?(GmailMessage)
+                              GmailMessage.new
+                            else
+                              ActiveRecord.const_get("GmailMessage").new
+                            end
+
+            pp "email.date: #{email.date}"
+            pp "email.uid: #{email.uid}"
+            pp "email.boundary: #{email.boundary}"
+            pp "email.to: #{email.to}"
+            pp "email.from:  #{email.from}"
+            pp "email.from:  #{from}"
+            pp "email.cc: #{email.cc}"
+            pp "email.bcc: #{email.bcc}"
+
+            gmail_message.date = email.date
+            gmail_message.uid = email.uid
+            gmail_message.boundary = email.boundary
+            gmail_message.to = email.to
+            gmail_message.from = email.from[0] || @user
+            gmail_message.cc = email.cc
+            gmail_message.bcc = email.bcc
+            gmail_message.raw_source = clean(email.raw_source)
+
+            if gmail_message.save
+              email.parts.each do |part|
+                gmail_message.parts.create(content: clean(part.body))
+              end
+            end
+
+          rescue => e
+            Rails.logger.error "Error saving , will ignore: #{e}"
+            pp "ERROR: #{e}"
+            pp gmail_message
+            next
+          end
+        end
+      end
+
+      def clean(source)
+        body = source.force_encoding('iso-8859-1').encode("UTF-8")
+        Sanitize.clean(body)
+      end
     end
   end
 
-  def process(messages)
-    messages.each do |email|
-      new_mail = Email.new
-      new_mail.uid = email.uid
-      new_mail.boundary = email.boundary
-      email.parts.each{|part| new_mail.parts.create(content: part.body)}
-      new_mail.to = email.to
-      new_mail.sender = email.sender || @user
-      new_mail.cc = email.cc
-      new_mail.bcc = email.bcc
-      new_mail.raw_source = new_mail.raw_source
-
-      new_mail.save
-    end
-  end
 end
